1e5e29ff45a5735185ea991b1972a4aa5245323e
Thomas R. Shemanske <thomas.r.shemanske@gmail.com>
2022-04-28 15:03:21 -0400

    Test SQL project

---
+++ shemanske-TEST-2022-04-28-14:22:34.sql
@@ -0,0 +1,45 @@
+INSERT INTO projects SET 
+project_xml_id = 'shemanske-TEST',
+project_title = 'Shemanske Test Project  \\( T:V \\to W \\)', 
+project_subtitle = '', 
+project_subject_id = '1', 
+project_target_level_id = '5', 
+project_phase_id = '1', 
+project_features = '1000010000101', 
+project_award_id = '', 
+project_license_code= 'public', 
+project_license_variant = '', 
+project_license_version = '', 
+project_recognition_code = NULL,
+project_publication_year = '2022', 
+project_price_amount = '0', 
+project_price_currency = '', 
+project_description_onesentence = 'This is a test project with math in the description
+\\( T: V \\to W \\)', 
+project_description_full = '<p>
+Longer description with display math.  Euler wrote:
+
+\\[ e^{i\\pi} + 1 = 0.\\]
+</p>', 
+project_landing_URL = 'https://math.dartmouth.edu/~trs', 
+project_source_URL = 'https://math.dartmouth.edu/~trs/', 
+project_html_URL = 'https://math.dartmouth.edu/~trs', 
+project_pdf_URL = 'https://math.dartmouth.edu/~trs', 
+project_print_URL = 'https://math.dartmouth.edu/~trs'; 
+
+INSERT INTO coauthors SET 
+coauthors_project_id = 'shemanske-TEST', 
+coauthors_displayname = 'Thomas R. Shemanske', 
+coauthors_url = 'https://math.dartmouth.edu/~trs', 
+coauthors_email = 'thomas.r.shemanske@gmail.com', 
+coauthors_affiliation = 'Dartmouth College', 
+coauthors_affiliation_2 = 'Town of Grantham, NH' ;
+
+INSERT INTO coauthors SET 
+coauthors_project_id = 'shemanske-TEST', 
+coauthors_displayname = 'David Farmer', 
+coauthors_url = '', 
+coauthors_email = 'farmer@aimath.org', 
+coauthors_affiliation = 'AIM', 
+coauthors_affiliation_2 = '' ;
+
